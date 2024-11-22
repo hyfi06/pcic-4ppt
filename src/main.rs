da3e14659499a675ac0007c31d51a9db82eeb46d
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2024-11-21 18:46:34 -0600

    test read file
    Signed-off-by: Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,13 @@
+use std::io;
 mod loader;
-fn main() {
-    println!("Hello, world!");
+fn main() -> io::Result<()> {
+    let filename = "otypes06.b08";
+    let point_count = 6;
+    let byte_size = 1;
+    
+    let point_sets = loader::load_file(filename, point_count, byte_size)?;
+    for (i, point_set) in point_sets.iter().enumerate() {
+        println!("Set {}: {:?}", i, point_set);
+    }
+    Ok(())
 }
